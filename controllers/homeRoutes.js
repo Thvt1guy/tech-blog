60aace2f58553fdd41cfe4dfadadd3fd1ee44d9f
Derrick Alcius <derrickalcius@gmail.com>
2023-04-19 20:48:35 -0400

    post now show on main page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
 // const { response } = require('express');
-// const {Product, ProductTag, Review, Tag, User, UserProduct} = require('../models/');
+const {Post} = require('../models/');
 
 // get all posts for homepage
 
 router.get('/', async (req, res) => {
     try {
         // res.status(200).json("Homepage Route Working!")
-        res.render('homepage', {user_id: req.session.user_id, logged_in: req.session.logged_in});
+        const postData = await Post.findAll();
+
+        const post = postData.map((post)=> post.get({ plain: true }));
+
+        res.render('homepage', { post, user_id: req.session.user_id, logged_in: req.session.logged_in});
     } catch(err) {
         res.status(500).json(err);
     }
